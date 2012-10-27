b5367a88a8a7de15195a03ec2141774885f8076d
Shinsuke Abe <mao.instantlife@gmail.com>
2012-10-27 11:38:02 +0900

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recordiet::Application.config.secret_token = '39e3d818a356c1e29582bc986249898f4fae16f2c557d24c574bfae602ceb4fcb1f3d52b62b222bc1cff38e8e8eb12dc14a24d0dbc8af31a955b7b642fd7fec3'
