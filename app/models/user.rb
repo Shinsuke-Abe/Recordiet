8ba4cb5120f7ceb02d0059b41d8739c68a838f75
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-01 00:04:53 +0900

    implements authenticate method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :display_name, :mail_address, :password
+  
+  def User.authenticate(mail_address, password)
+    User.find(:first, :conditions => {:mail_address => mail_address, :password => password})
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,23 @@
+# encoding: utf-8
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test "認証要求でメールアドレスとパスワードの両方が一致する場合はユーザ情報を返却する" do
+    auth_user = User.authenticate("john@mail.com", "pass1234")
+    assert_not_nil auth_user
+    assert_equal auth_user.mail_address, "john@mail.com"
+    assert_equal auth_user.display_name, "john denver"
+    assert_equal auth_user.password, "pass1234"
+  end
+  
+  test "認証要求でパスワードが一致しない場合はnilを返す" do
+    assert_nil User.authenticate("john@mail.com", "pass9876")
+  end
+  
+  test "認証要求でメールアドレスが一致しない場合はnilを返す" do
+    assert_nil User.authenticate("nemo@mail.com", "pass1234")
+  end
+  
   # test "the truth" do
   #   assert true
   # end
