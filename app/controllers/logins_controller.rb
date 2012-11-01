524880effbbcd313c07cc64b49ae7d3da7cb1731
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-01 23:37:37 +0900

    implements action when input incorrect user authenticate information.

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,11 +1,17 @@
-# encofing: utf-8
+# encoding: utf-8
 class LoginsController < ApplicationController
   def show
     @user = User.new
   end
   
   def create
-    session[:mail_address] = "john@mail.com"
-    redirect_to user_path
+    @user = User.new(params[:user])
+    if User.authenticate(@user.mail_address, @user.password)
+      session[:mail_address] = "john@mail.com"
+      redirect_to user_path
+    else
+      flash[:notice] = "メールアドレスかパスワードが間違っています。"
+      render :action => "show"
+    end
   end
 end
--- test/functional/logins_controller_test.rb
+++ test/functional/logins_controller_test.rb
@@ -9,13 +9,15 @@ class LoginsControllerTest < ActionController::TestCase
   
   test "正しいユーザ名とパスワードを入力すると履歴画面に遷移する" do
     post :create, :user => {:mail_address => "john@mail.com", :password => "pass1234"}
+    
     assert_redirected_to user_path
     assert_equal session[:mail_address], "john@mail.com"
   end
   
   test "間違ったユーザ名とパスワードを入力するとログインフォームを再表示する" do
     post :create, :user => {:mail_address => "nemo@mail.com", :password => "noname1234"}
-    assert_redirected_to login_path
+    
+    assert_response :success
     assert_equal "メールアドレスかパスワードが間違っています。", flash[:notice]
   end
   #
