e41992dca009e7d107f89d217ce0ff6d3b74dc27
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-03 08:55:36 +0900

    add test: register user and change priority.

--- test/integration/weight_log_flow_test.rb
+++ test/integration/weight_log_flow_test.rb
@@ -9,18 +9,36 @@ class WeightLogFlowTest < ActionDispatch::IntegrationTest
     get "/login"
     assert_response :success
     
-    post_via_redirect "/login", :user => {:mail_address => "john@mail.com", :password => "pass1234"}
+    post_via_redirect "/login", :user => {
+      :mail_address => "john@mail.com",
+      :password => "pass1234"}
     assert_equal "/user", path
     assert_equal "履歴が未登録です。", flash[:notice]
   end
   
-  test "履歴登録済：ログインすると履歴ページに一覧を表示する" do
+  test "ユーザ登録が成功する" do
     https!
-    post_via_redirect "/login", :user => {:mail_address => "eric@mail.com", :password => "pass9876"}
-    assert_equal "/user", path
+    get "/user/new"
+    assert_response :success
     assert assigns(:user)
-    assert assigns(:user => :weight_logs)
+    
+    post_via_redirect "/user/create", :user => {
+      :mail_address => "jimmy@ledzeppelin.com",
+      :display_name => "jimmy page",
+      :password => "guitar"}
+    assert_equal "/user", path
+    assert_equal "履歴が未登録です。", flash[:notice]
   end
+  
+  # test "履歴登録済：ログインすると履歴ページに一覧を表示する" do
+    # https!
+    # post_via_redirect "/login", :user => {
+      # :mail_address => "eric@mail.com",
+      # :password => "pass9876"}
+    # assert_equal "/user", path
+    # assert assigns(:user)
+    # assert assigns(:user => :weight_logs)
+  # end
   
   # test "the truth" do
   #   assert true
