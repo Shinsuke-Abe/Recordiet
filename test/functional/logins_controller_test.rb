b51b94b3808d2b5ebd62fb39b8158170efa514d5
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-03 19:14:22 +0900

    refactoring test code.

--- test/functional/logins_controller_test.rb
+++ test/functional/logins_controller_test.rb
@@ -10,22 +10,21 @@ class LoginsControllerTest < ActionController::TestCase
   end
   
   test "正しいユーザ名とパスワードを入力すると履歴画面に遷移する" do
-    post :create, :user => {:mail_address => users(:one).mail_address, :password => users(:one).password}
+    one = users(:one)
+    post :create, :user => {
+      :mail_address => one.mail_address,
+      :password => one.password}
     
     assert_redirected_to user_path
-    assert_equal session[:id], users(:one).id
+    assert_equal session[:id], one.id
   end
   
   test "間違ったユーザ名とパスワードを入力するとログインフォームを再表示する" do
-    post :create, :user => {:mail_address => "nemo@mail.com", :password => "noname1234"}
+    post :create, :user => {
+      :mail_address => "nemo@mail.com",
+      :password => "noname1234"}
     
     assert_response :success
     assert_equal "メールアドレスかパスワードが間違っています。", flash[:notice]
   end
-  #
-  
-  # 
-  # test "the truth" do
-  #   assert true
-  # end
 end
