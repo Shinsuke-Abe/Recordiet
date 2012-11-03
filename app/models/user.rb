7b717f8f77bd2adba7e115ff30e6e538867432e8
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-03 02:00:52 -0400

    associating user and weight_log model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :display_name, :mail_address, :password
   
+  has_many :weight_logs
+  
   def User.authenticate(mail_address, password)
     User.find(:first, :conditions => {:mail_address => mail_address, :password => password})
   end
