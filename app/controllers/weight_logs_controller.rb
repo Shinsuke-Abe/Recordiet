2721234e951e10b7710dfe15d948e04833d77787
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-04 19:17:09 +0900

    red: fail create weight log test.

--- app/controllers/weight_logs_controller.rb
+++ app/controllers/weight_logs_controller.rb
@@ -2,7 +2,7 @@ class WeightLogsController < ApplicationController
   # add weight logs to login user
   def create
     @user = User.find(session[:id])
-    @weight_log = @user.weight_logs.create(params[:weight_log])
+    @weight_log = @user.weight_logs.create!(params[:weight_log])
     redirect_to user_path
   end
 end
--- test/functional/weight_logs_controller_test.rb
+++ test/functional/weight_logs_controller_test.rb
@@ -16,6 +16,19 @@ class WeightLogsControllerTest < ActionController::TestCase
     assert_equal Date.today - 1, john_log_added.weight_logs[0].measured_date
     assert_equal 70.9, john_log_added.weight_logs[0].weight
   end
+  
+  test "計測日未指定の場合はエラーメッセージを表示する" do
+    session[:id] = users(:eric).id
+    post :create, :weight_log => {
+      :weight => 55.5
+    }
+    
+    eric_log_not_added = assigns(:user)
+    p users(:eric).weight_logs
+    p eric_log_not_added.weight_logs
+    assert_equal users(:eric).weight_logs.length, eric_log_not_added.weight_logs.length
+    assert_equal "記録の登録には計測日と体重が必要です。", flash[:notice]
+  end
   # test "the truth" do
   #   assert true
   # end
