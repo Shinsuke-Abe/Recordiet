134b25584dcdd48f37b578560c1723ac897a351e
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-06 00:31:33 +0900

    refactoring weight_logs_controller_test

--- test/functional/weight_logs_controller_test.rb
+++ test/functional/weight_logs_controller_test.rb
@@ -5,26 +5,20 @@ class WeightLogsControllerTest < ActionController::TestCase
   fixtures :users, :weight_logs
   
   test "ログイン済のユーザに履歴を登録することができる" do
-    john_log_added = register_weight_log_action(:john, Date.today - 1, 70.9)
-#    session[:id] = users(:john).id
-#    post :create, :weight_log => {
-#      :measured_date => ,
-#      :weight => 
-#    }
-#    
-#    john_log_added = assigns(:user)
+    expected_data = { :measured_date => Date.today - 1, :weight => 70.9}
+    john_log_added = register_weight_log_action(
+      :john,
+      expected_data[:measured_date],
+      expected_data[:weight])
+
     assert_equal 1, john_log_added.weight_logs.length
-    assert_equal Date.today - 1, john_log_added.weight_logs[0].measured_date
-    assert_equal 70.9, john_log_added.weight_logs[0].weight
+    assert_equal expected_data[:measured_date], john_log_added.weight_logs[0].measured_date
+    assert_equal expected_data[:weight], john_log_added.weight_logs[0].weight
   end
   
   test "計測日未指定の場合はエラーメッセージを表示する" do
-    session[:id] = users(:eric).id
-    post :create, :weight_log => {
-      :weight => 55.5
-    }
+    eric_log_not_added = register_weight_log_action(:john, nil, 55.5)
     
-    eric_log_not_added = assigns(:user)
     assert_equal(
       users(:eric).weight_logs.length,
       User.find(users(:eric).id).weight_logs.length)
@@ -32,30 +26,22 @@ class WeightLogsControllerTest < ActionController::TestCase
   end
   
   test "指定したログの内容を修正することができる" do
-    session[:id] = users(:eric).id
-    put :update, {
-      :id => users(:eric).weight_logs[1].id,
-      :weight_log =>{
-        :measured_date => Date.yesterday,
-        :weight => 69.0
-      }}
+    expected_data = {:measured_date => Date.yesterday, :weight => 69.0}
+    update_weight_log_action(
+      :eric,
+      expected_data[:measured_date],
+      expected_data[:weight])
     
     weight_log_of_eric = User.find(users(:eric).id).weight_logs
     updated_log = weight_log_of_eric[
       weight_log_of_eric.index{|weight_log|
         weight_log.id == users(:eric).weight_logs[1].id}] 
-    assert_equal Date.yesterday, updated_log.measured_date
-    assert_equal 69.0, updated_log.weight
+    assert_equal expected_data[:measured_date], updated_log.measured_date
+    assert_equal expected_data[:weight], updated_log.weight
   end
   
   test "ログの変更内容の体重が未入力の場合はエラーメッセージを表示する" do
-    session[:id] = users(:eric).id
-    put :update, {
-      :id => users(:eric).weight_logs[1].id,
-      :weight_log =>{
-        :measured_date => Date.yesterday,
-        :weight => nil
-      }}
+    update_weight_log_action(:eric, Date.yesterday)
       
     assert_equal "記録の登録には計測日と体重が必要です。", flash[:notice]
   end
@@ -69,5 +55,15 @@ class WeightLogsControllerTest < ActionController::TestCase
     }
     
     assigns(:user)
+  end
+  
+  def update_weight_log_action(name, measured_date = nil, weight = nil)
+    session[:id] = users(name).id
+    put :update, {
+      :id => users(name).weight_logs[1].id,
+      :weight_log =>{
+        :measured_date => measured_date,
+        :weight => weight
+      }}
   end
 end
