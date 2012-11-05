88ad1445cb706d72766747154254fc461b247157
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-04 19:54:21 -0500

    refactoring test code. and test code bugfix to form data added to weight_logs.

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,20 +2,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  fixtures :users, :weight_logs
   
   test "show:履歴を持たないユーザを表示する" do
     assigned_john = show_user_action(:john)
     
-    assert_not_nil assigned_john
-    assert_equal assigned_john, users(:john)
-    assert assigned_john.weight_logs.empty?
+    assert take_off_form_data(assigned_john.weight_logs).empty?
     assert_equal "履歴が未登録です。", flash[:notice]
   end
   
   test "show:履歴を持つユーザを表示する" do
     assigned_eric = show_user_action(:eric)
     
-    assert_not_nil assigned_eric
     assert_nil flash[:notice]
     assert_weight_logs users(:eric), assigned_eric
   end
@@ -27,23 +25,14 @@ class UsersControllerTest < ActionController::TestCase
     get :show
     assert_response :success
     
-    assigns(:user)
+    assigned_user = assigns(:user)
+    
+    assert_not_nil assigned_user
+    assert_equal users(name), assigned_user
+    
+    assigned_user
   end
-  # setup do
-    # @user = users(:one)
-  # end
-# 
-  # test "should get index" do
-    # get :index
-    # assert_response :success
-    # assert_not_nil assigns(:users)
-  # end
-# 
-  # test "should get new" do
-    # get :new
-    # assert_response :success
-  # end
-# 
+
   # test "should create user" do
     # assert_difference('User.count') do
       # post :create, user: { display_name: @user.display_name, mail_address: @user.mail_address, password: @user.password }
@@ -51,22 +40,7 @@ class UsersControllerTest < ActionController::TestCase
 # 
     # assert_redirected_to user_path(assigns(:user))
   # end
-# 
-  # test "should show user" do
-    # get :show, id: @user
-    # assert_response :success
-  # end
-# 
-  # test "should get edit" do
-    # get :edit, id: @user
-    # assert_response :success
-  # end
-# 
-  # test "should update user" do
-    # put :update, id: @user, user: { display_name: @user.display_name, mail_address: @user.mail_address, password: @user.password }
-    # assert_redirected_to user_path(assigns(:user))
-  # end
-# 
+  
   # test "should destroy user" do
     # assert_difference('User.count', -1) do
       # delete :destroy, id: @user
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,10 +12,17 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   
   def assert_weight_logs(expect_user, actual_user)
-    assert_equal expect_user.weight_logs.length, actual_user.weight_logs.length
+    actual_weight_logs = take_off_form_data(actual_user.weight_logs)
+    assert_equal expect_user.weight_logs.length, actual_weight_logs.length
     
     for weight_log in expect_user.weight_logs do
-      assert actual_user.weight_logs.include?(weight_log)
+      assert actual_weight_logs.include?(weight_log)
     end
+  end
+  
+  def take_off_form_data(weight_logs)
+    weight_logs.select { |log|
+      log.id != nil
+    }
   end
 end
