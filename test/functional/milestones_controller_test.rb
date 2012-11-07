450ccf03b52209b0eeec91271532f3e8f76a4096
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-06 22:54:28 -0500

    add test create new milestone

--- test/functional/milestones_controller_test.rb
+++ test/functional/milestones_controller_test.rb
@@ -10,4 +10,17 @@ class MilestonesControllerTest < ActionController::TestCase
     assigns(:user)
     assigns(:milestone)
   end
+  
+  test "目標を新規保存する" do
+    session[:id] = users(:john)
+    post :create, :milestone => {
+      :weight => 64.0,
+      :date => Date.today + 60.days,
+      :reward => "ケーキバイキング"}
+    
+    johns_milestone = User.find(users(:john).id).milestone
+    assert_equal 64.0, johns_milestone.weight
+    assert_equal Date.today + 60.days, johns_milestone.date
+    assert_equal "ケーキバイキング", johns_milestone.reward
+  end
 end
