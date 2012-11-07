8ae243f1fee704fe50768af8684248ec2b60ffc9
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-06 19:20:38 -0500

    add test milestone controller new method

--- test/functional/milestones_controller_test.rb
+++ test/functional/milestones_controller_test.rb
@@ -1,6 +1,13 @@
+# encoding: utf-8
 require 'test_helper'
 
 class MilestonesControllerTest < ActionController::TestCase
+
+  test "目標入力フォームを開く" do
+    get :new
+    
+    assigns(:milestone)
+  end
   # test "the truth" do
   #   assert true
   # end
