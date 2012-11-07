9669104342b5c61b5dcb0dc48874a96dbc9844ea
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-07 23:52:45 +0900

    issue #29 : add test for weight_logs default sort.

--- test/unit/weight_log_test.rb
+++ test/unit/weight_log_test.rb
@@ -25,4 +25,19 @@ class WeightLogTest < ActiveSupport::TestCase
     )
     assert new_weight_log.invalid?
   end
+  
+  test "体重履歴は日付の降順にソートされる" do
+    eric = users(:eric)
+    eric.weight_logs.create(
+      :measured_date => Date.yesterday,
+      :weight => 69.0
+    )
+    
+    log_added_eric = User.find(users(:eric).id)
+    
+    assert_equal 3, log_added_eric.weight_logs.length
+    assert_equal Date.yesterday, log_added_eric.weight_logs[0].measured_date
+    assert_equal weight_logs[:two].measured_date, log_added_eric.weight_logs[1].measured_date
+    assert_equal weight_logs[:one].measured_date, log_added_eric.weight_logs[2].measured_date
+  end
 end
