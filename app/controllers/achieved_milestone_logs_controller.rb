580decd95671748a82574c4ae0f3068828c57356
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-17 21:51:11 +0900

    bugfix achieved milestone not found notice message displayed in weight_logs page.

--- app/controllers/achieved_milestone_logs_controller.rb
+++ app/controllers/achieved_milestone_logs_controller.rb
@@ -2,6 +2,7 @@
 class AchievedMilestoneLogsController < ApplicationController
   include AchievedMilestoneLogsHelper
   before_filter :required_login
+  after_filter :flash_clear
   
   def index
     @achieved_milestone_logs = @user.achieved_milestone_logs
@@ -9,5 +10,10 @@ class AchievedMilestoneLogsController < ApplicationController
     if !@achieved_milestone_logs or @achieved_milestone_logs.empty?
       flash[:notice] = AchievedMilestoneLogsHelper::ACHIEVED_MILESTONE_NOT_FOUND
     end
+  end
+  
+  private 
+  def flash_clear
+    flash[:notice] = nil
   end
 end
