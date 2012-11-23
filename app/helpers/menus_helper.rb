5f427b832b8eed9a5daee34b3071ac51af528751
Shinsuke Abe <mao.instantlife@gmail.com>
2012-11-23 21:14:14 +0900

    issue #57 : move menu helpers method to menu model class method.

--- app/helpers/menus_helper.rb
+++ app/helpers/menus_helper.rb
@@ -1,21 +1,3 @@
 # encoding: utf-8
 module MenusHelper
-
-  def display_menu_type(type)
-    menu_type_hash[type]
-  end
-  
-  def menu_type_hash
-    {
-      1 => "朝食",
-      2 => "昼食",
-      3 => "夕食",
-      4 => "間食",
-      5 => "その他"
-    }
-  end
-  
-  def select_menu_array
-    menu_type_hash.invert.to_a
-  end
 end
