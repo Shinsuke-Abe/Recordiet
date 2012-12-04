f3d3aa4bd3023fe03d9249ec62933a4c919e0946
Shinsuke Abe <mao.instantlife@gmail.com>
2012-12-03 22:57:00 -0500

    refactoring create_chart method and add test(assertion is not refactored)

--- app/helpers/weight_logs_helper.rb
+++ app/helpers/weight_logs_helper.rb
@@ -2,17 +2,17 @@
 require 'gchart'
 
 module WeightLogsHelper
-  def create_chart(data_legend)
-    unless current_user.weight_logs.empty?
-      data_arr, axis_arr = chart_arrays current_user.weight_logs do |weight_log|
+  def create_chart(user, data_legend)
+    unless user.weight_logs.empty?
+      data_arr, axis_arr = chart_arrays user.weight_logs do |weight_log|
         yield weight_log
       end
       
       chart_arg = chart_basic(data_arr, axis_arr)
       
-      if current_user.milestone and
-         yield current_user.milestone
-        milestone = yield current_user.milestone
+      if user.milestone and
+         yield user.milestone
+        milestone = yield user.milestone
         chart_arg[:data] << Array.new(data_arr.size, milestone)
         chart_arg[:bar_colors] += ",FF99CC"
         chart_arg[:legend] = [data_legend, "目標"]
@@ -39,23 +39,8 @@ module WeightLogsHelper
     
     chart_arg[:axis_labels] = [axis_arr]
     chart_arg[:axis_with_labels] = ["x", "y"]
-    chart_arg[:axis_range] = [nil, [0, data_arr.max{|a,b| compare_chart_data(a,b)}, 5]]
+    chart_arg[:axis_range] = [nil, [0, data_arr.select{ |data| data}.max, 5]]
     
     chart_arg
-  end
-  
-  # データに抜けがある場合のmax値取得のための対処
-  def compare_chart_data(a, b)
-    if !a and !b
-      0
-    else
-      if !a
-        -1
-      elsif !b
-        1
-      else
-        a <=> b
-      end
-    end
   end
 end
--- test/unit/helpers/weight_logs_helper_test.rb
+++ test/unit/helpers/weight_logs_helper_test.rb
@@ -1,4 +1,17 @@
+# encoding: utf-8
 require 'test_helper'
 
 class WeightLogsHelperTest < ActionView::TestCase
+  fixtures :all
+  test "目標のある体重履歴でグラフを作成する" do
+    eric = users(:eric)
+    chart_url = create_chart(eric, "体重") { |data| data.weight }
+    
+    assert chart_url.include? sprintf("chxl=0:|%s|%s", weight_logs(:one).measured_date.month.to_s + "%2F" + weight_logs(:one).measured_date.day.to_s, weight_logs(:two).measured_date.month.to_s + "%2F" + weight_logs(:two).measured_date.day.to_s)
+    assert chart_url.include? "chco=3300FF,FF99CC"
+    assert chart_url.include? "chds=0,75.0"
+    assert chart_url.include? sprintf("chdl=%s|%s", URI.escape("体重"), URI.escape("目標"))
+    assert chart_url.include? "chxr=1,0,75.0,5"
+    assert chart_url.include? "chd=t:74.9,75.0|1.5,1.5"
+  end
 end
