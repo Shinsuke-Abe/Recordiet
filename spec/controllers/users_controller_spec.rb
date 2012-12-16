4f5190a14f5d59091b80fec2fd54116d7fa16e29
Shinsuke Abe <mao.instantlife@gmail.com>
2012-12-16 20:33:00 +0900

    add user post test.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,20 +9,43 @@ describe UsersController do
         :display_name => "new user",
         :password => "pass9876"
       }
-      
+
       response.should redirect_to weight_logs_path
     end
-    
+
     it "ユーザの新規登録でエラーになる場合は新規登録フォームが再表示される" do
       post :create, :user => {
         :mail_address => nil
       }
-      
+
       response.should render_template "new"
     end
   end
-  
-  describe "PUT user" do
 
+  describe "PUT user" do
+    before do
+      john = FactoryGirl.create(:john)
+      session[:id] = john.id
+    end
+
+    it "ユーザの情報が正常に変更できた場合は体重履歴ページにリダイレクトされる" do
+      put :update, :user => {
+        :mail_address => "newuser@mail.com",
+        :display_name => "neweric dominos",
+        :password => "pass2345"
+      }
+
+      response.should redirect_to weight_logs_path
+    end
+
+    it "ユーザ情報の変更でエラーになる場合は変更フォームが再表示される" do
+      put :update, :user => {
+        :mail_address => nil,
+        :display_name => "neweric",
+        :password => "pass1234"
+      }
+
+      response.should render_template "edit"
+    end
   end
 end
