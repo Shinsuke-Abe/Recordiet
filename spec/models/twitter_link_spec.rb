bcf9f0f05c4a38e7d8234f659ff71dac38c9cddb
Shinsuke Abe <mao.instantlife@gmail.com>
2013-01-11 00:50:01 +0900

    add twitter_link model validates consumer_key spec.

--- spec/models/twitter_link_spec.rb
+++ spec/models/twitter_link_spec.rb
@@ -1,5 +1,13 @@
+# encoding:utf-8
 require 'spec_helper'
 
 describe TwitterLink do
+  describe ".invalid" do
+    it "コンシューマーキーが未入力" do
+      twitter_link = TwitterLink.new(
+        :consumer_secret => "secret")
 
+      expect(twitter_link.invalid?).to be_true
+    end
+  end
 end
